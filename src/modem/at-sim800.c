70f65914a0941373ce1157699cf20867e987e9c4
jiandeng <jiandeng.develop@gmail.com>
2016-10-11 15:54:40 +0800

    Enable full functionality in case it is disabled

--- src/modem/at-sim800.c
+++ src/modem/at-sim800.c
@@ -197,6 +197,7 @@ static int sim800_attach(struct cellular *modem)
     static const char *const init_strings[] = {
 //        "AT+IPR=0",                     /* Enable autobauding if not already enabled. */
 //        "AT+IFC=0,0",                   /* Disable hardware flow control. */
+        "AT+CFUN=1",                    /* Enable full functionality. */
         "AT+CMEE=2",                    /* Enable extended error reporting. */
         "AT+CLTS=0",                    /* Don't sync RTC with network time, it's broken. */
         "AT+CIURC=0",                   /* Disable "Call Ready" URC. */

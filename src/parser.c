2bc7033407ec79a4f242746e654a14f08846fe0c
Jiandeng <jiandeng.develop@gmail.com>
2018-12-14 19:01:49 +0800

    Silly support for log

--- src/parser.c
+++ src/parser.c
@@ -83,6 +83,7 @@ void at_parser_show_residual(struct at_parser *parser)
     if(len > 0) {
         parser->buf[parser->buf_used]= 0;
         DBG_D(">! %s (%d)\r\n", parser->buf + parser->buf_current, len);
+        LOG_AT(">! %s (%d)\r\n", parser->buf + parser->buf_current, len);
 #ifdef DBG_F
         DBG_F();
 #endif
@@ -189,10 +190,13 @@ static void parser_handle_line(struct at_parser *parser)
     /* Log the received line. */
     if(parser->state == STATE_HEXDATA) {
         DBG_D("H> [%d bytes]\r\n", len);
+        LOG_AT("H> [%d bytes]\r\n", len);
     } else if(parser->state == STATE_RAWDATA) {
         DBG_D("R> [%d bytes]\r\n", len);
+        LOG_AT("R> [%d bytes]\r\n", len);
     } else {
         DBG_D(">> %s\r\n", line);
+        LOG_AT(">> %s\r\n", line);
     }
 #ifdef DBG_F
     DBG_F();
